038e25fdeeda204d33b2412e19b2ff8b21d6973b
Jake Vanderkraan <vdkjake@gmail.com>
2020-10-22 19:15:36 +1100

    Base commit Add skeeton model w/ animations Add run, idle animations (+ a few extras ;)) Set up basic scene w/ trees Add movement & rotation script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private PlayerRotation pr;
+
+    [SerializeField] private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMove = Input.GetAxis("Horizontal") * moveSpeed;
+        float zMove = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 movement = new Vector3(xMove, 0f, zMove);
+        cc.Move(movement * Time.deltaTime);
+
+        if (xMove != 0 || zMove != 0)
+        {
+            pr.Rotate(movement);
+            anim.SetBool("Move", true);
+        }
+        else
+        {
+            anim.SetBool("Move", false);
+        }
+
+        if(Random.Range(1, 100) == 1)
+        {
+            anim.SetTrigger("IdleRandom");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    private Quaternion targetRotation;
+
+    [SerializeField] private float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotateSpeed);
+    }
+
+    public void Rotate(Vector3 movement)
+    {
+        targetRotation = Quaternion.LookRotation(movement);
+    }
+}
