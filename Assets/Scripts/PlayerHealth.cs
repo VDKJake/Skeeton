82515bb8c4fffe4ee7fda33777f553438c135cee
Jake Vanderkraan <vdkjake@gmail.com>
2020-10-22 23:24:59 +1100

    Adjust tree materials Add ability to see skeeton from behind other models Change trees to have a random rotation Start on health system

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health;
+    [SerializeField] List<GameObject> bodyParts = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            ChangeHealth(-1);
+        }
+    }
+
+    public void ChangeHealth(int amt)
+    {
+        health += amt;
+        
+        for(int i = bodyParts.Count - 1; i >= health; i--)
+        {
+            bodyParts[i].SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/TreeRotation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRotation : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.transform.Rotate(0f, Random.Range(0, 360), 0f);
+    }
+}
